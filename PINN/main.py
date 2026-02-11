52531243dc3f839570d47aa124c41f9c7a56edc5
minhtam04-byte <minhtam222004@gmail.com>
2026-02-11 22:57:59 +0700

    main.py

--- PINN/main.py
+++ PINN/main.py
@@ -15,19 +15,17 @@ class PorousInversePINN(PINN):
         self.R = 0.5
 
     def u_net(self, params, x, y, t):
-        """Dự đoán vận tốc (u, v) và áp suất (p)"""
         X = jnp.concatenate([t, x, y], axis=-1)
         X_norm = 2.0 * (X - self.lb) / (self.ub - self.lb) - 1.0
         outputs = self.state.apply_fn(params, X_norm)
         return outputs[..., 0], outputs[..., 1], outputs[..., 2]
 
     def r_net(self, params, x, y, t):
-        """Phần dư phương trình tại 1 điểm đơn lẻ (x, y, t)"""
-        def get_u(x, y, t): return self.u_net(params, x, y, t)[0]
-        def get_v(x, y, t): return self.u_net(params, x, y, t)[1]
-        def get_p(x, y, t): return self.u_net(params, x, y, t)[2]
+        def get_u(x, y, t): return self.u_net(params, x, y, t)[0].reshape(())
+        def get_v(x, y, t): return self.u_net(params, x, y, t)[1].reshape(())
+        def get_p(x, y, t): return self.u_net(params, x, y, t)[2].reshape(())
 
-        # Đạo hàm bậc 1 & 2 
+        # Đạo hàm bậc 1
         u_t = grad(get_u, argnums=2)(x, y, t)
         u_x = grad(get_u, argnums=0)(x, y, t)
         u_y = grad(get_u, argnums=1)(x, y, t)
@@ -37,6 +35,7 @@ class PorousInversePINN(PINN):
         p_x = grad(get_p, argnums=0)(x, y, t)
         p_y = grad(get_p, argnums=1)(x, y, t)
 
+        # Đạo hàm bậc 2
         u_xx = grad(lambda x,y,t: grad(get_u, argnums=0)(x,y,t))(x, y, t)
         u_yy = grad(lambda x,y,t: grad(get_u, argnums=1)(x,y,t))(x, y, t)
         v_xx = grad(lambda x,y,t: grad(get_v, argnums=0)(x,y,t))(x, y, t)
@@ -45,6 +44,8 @@ class PorousInversePINN(PINN):
         u_val, v_val, _ = self.u_net(params, x, y, t)
         is_porous = jnp.where(x**2 + y**2 <= self.R**2, 1.0, 0.0)
         
+        # Phương trình Navier-Stokes + Darcy:
+        # f_u = u_t + (u \cdot \nabla) u + p_x - \frac{1}{Re} \nabla^2 u + \frac{1}{Da} u \cdot \mathbb{I}_{porous}
         f_u = u_t + (u_val*u_x + v_val*u_y) + p_x - (1.0/self.Re)*(u_xx + u_yy) + (1.0/self.Da)*u_val*is_porous
         f_v = v_t + (u_val*v_x + v_val*v_y) + p_y - (1.0/self.Re)*(v_xx + v_yy) + (1.0/self.Da)*v_val*is_porous
         f_e = u_x + v_y
@@ -55,12 +56,12 @@ class PorousInversePINN(PINN):
         t_d, x_d, y_d, u_d, v_d = batch['data']
         t_e, x_e, y_e = batch['eqn']
         
-        # 1. Loss dữ liệu
         u_p, v_p, _ = self.u_net(params, x_d, y_d, t_d)
+        
+        # SỬA LỖI: Squeeze và reshape để đảm bảo kết quả là () thay vì (1,)
         loss_data = jnp.mean(jnp.square(u_p.squeeze() - u_d.squeeze()) + 
                              jnp.square(v_p.squeeze() - v_d.squeeze()))
         
-        # 2. Loss vật lý
         f_u, f_v, f_e = vmap(self.r_net, (None, 0, 0, 0))(params, 
                                                          x_e.squeeze(), 
                                                          y_e.squeeze(), 
